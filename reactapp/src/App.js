b7e4ca0b4db76755e12f0e72138060a96965e9d6
epaavola <paavola.esa@gmail.com>
2020-02-03 10:54:47 +0200

    Added Dashboard page. Added Axios, Formik and Router dependencies. Added functionality to the navbar component. Added Service folder and empty data files for future use.

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Frontpage from './pages/frontpage'
+import Dashboard from './pages/Dashboard'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Frontpage />
+    <Router>
+                <>
+                    <Switch>
+                        <Route path="/" exact component={Frontpage} />
+                        <Route path="/dashboard" exact component={Dashboard} />
+                    </Switch>
+                </>
+    </Router>
   )
 }
 
---
+++ reactapp/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'typeface-roboto'
+import Navbar from '../components/navbar'
+
+class Dashboard extends Component {
+    render() {
+      return (
+        <Navbar />
+      );
+    }
+  }
+
+export default Dashboard
