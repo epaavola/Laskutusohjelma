cad6d1e8e45feee1be1c035225cb4c088915e3c4
samitup <sami.tuppurainen@metropolia.fi>
2020-04-30 17:37:03 +0300

    Testejä lisätty

--- LaskutusApplication/src/test/java/fi/metropolia/LaskutusApplication/dto/CompanyDTOTest.java
+++ LaskutusApplication/src/test/java/fi/metropolia/LaskutusApplication/dto/CompanyDTOTest.java
@@ -1,13 +1,44 @@
 package fi.metropolia.LaskutusApplication.dto;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.metropolia.LaskutusApplication.dao.CompanyDao;
+import fi.metropolia.LaskutusApplication.model.DAOCompany;
+
+@RunWith(SpringRunner.class)
 class CompanyDTOTest {
+	@TestConfiguration
+	static class CompanyServiceImplTestContextConfiguration {
 
-    private CompanyDTO companyDTOUnderTest;
+	}
 
-    @BeforeEach
-    void setUp() {
-        companyDTOUnderTest = new CompanyDTO("company", "vatID", "name", "address", "city", "email");
-    }
+
+	@MockBean
+	private CompanyDao companyRepository;
+
+	@Before
+	public void setUp() {
+		DAOCompany alex = new DAOCompany();
+		alex.setName("alex");
+
+		Mockito.when(companyRepository.findByCompany(alex.getName())).thenReturn(alex);
+	}
+
+	@Test
+	public void whenValidName_thenCompanyShouldBeFound() {
+		String name = "alex";
+		DAOCompany found = companyRepository.findByCompany(name);
+
+		assertThat(found.getName()).isEqualTo(name);
+	}
 }
