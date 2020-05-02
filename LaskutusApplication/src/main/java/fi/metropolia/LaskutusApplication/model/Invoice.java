e96c5a3dc6bc4f5babbb18a8d6146c0ed70c11a9
samitup <sami.tuppurainen@metropolia.fi>
2020-05-02 23:23:57 +0300

    added product-model, InvoiceLine and pdfView

--- LaskutusApplication/src/main/java/fi/metropolia/LaskutusApplication/model/Invoice.java
+++ LaskutusApplication/src/main/java/fi/metropolia/LaskutusApplication/model/Invoice.java
@@ -1,22 +1,37 @@
 package fi.metropolia.LaskutusApplication.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.util.Set;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 @Entity
-public class Invoice {
+@Table(name="invoices")
+public class Invoice implements Serializable {
 
-	private Long id;
-	private String invoiceNumber;
-
-	private Double netAmount;	//Netto
-	private Double grossAmount; //Brutto
-	private Double vatAmount; // alvi
+	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+
+	private String invoiceNumber;
+
+	private Double netAmount; // Netto
+	private Double grossAmount; // Brutto
+	private Double vatAmount; // alvi
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Company company;
+
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL, orphanRemoval=true)
+	@JoinColumn(name="invoice_id")
+	private List<InvoiceLine> lines;
+	
+	public Invoice() {
+		this.lines = new ArrayList<>();
+	}
 	public Long getId() {
 		return id;
 	}
@@ -55,6 +70,34 @@ public class Invoice {
 
 	public void setVatAmount(Double vatAmount) {
 		this.vatAmount = vatAmount;
+	}
+
+	public Company getClient() {
+		return company;
+	}
+
+	public void setClient(Company company) {
+		this.company = company;
+	}
+
+	public List<InvoiceLine> getLines() {
+		return lines;
+	}
+
+	public void setLines(List<InvoiceLine> lines) {
+		this.lines = lines;
+	}
+
+	public void addLine(InvoiceLine line) {
+		this.lines.add(line);
+	}
+
+	public Double getTotal() {
+		Double total = 0.0;
+		for (InvoiceLine line : lines) {
+			total += line.calculatePrice();
+		}
+		return total;
 	}
 
 }
