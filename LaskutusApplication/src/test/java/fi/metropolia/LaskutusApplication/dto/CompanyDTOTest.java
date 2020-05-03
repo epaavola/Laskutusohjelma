59f561646712b7b5249e7a98288343f12c92c3c8
esapa <paavola.esa@gmail.com>
2020-05-03 11:01:51 +0300

    pom.xml added Lowagie dependency. Tests: junit4 -> junit5 conversions

--- LaskutusApplication/src/test/java/fi/metropolia/LaskutusApplication/dto/CompanyDTOTest.java
+++ LaskutusApplication/src/test/java/fi/metropolia/LaskutusApplication/dto/CompanyDTOTest.java
@@ -1,21 +1,21 @@
 package fi.metropolia.LaskutusApplication.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import fi.metropolia.LaskutusApplication.dao.CompanyDao;
 import fi.metropolia.LaskutusApplication.model.DAOCompany;
 
-@RunWith(SpringRunner.class)
+
+@ExtendWith(SpringExtension.class)
 public class CompanyDTOTest {
 	@TestConfiguration
 	static class CompanyServiceImplTestContextConfiguration {
@@ -26,7 +26,7 @@ public class CompanyDTOTest {
 	@MockBean
 	private CompanyDao companyRepository;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		DAOCompany alex = new DAOCompany();
 		alex.setName("alex");
--- LaskutusApplication/src/test/java/fi/metropolia/LaskutusApplication/dto/UserDTOTest.java
+++ LaskutusApplication/src/test/java/fi/metropolia/LaskutusApplication/dto/UserDTOTest.java
@@ -3,22 +3,24 @@ package fi.metropolia.LaskutusApplication.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 import fi.metropolia.LaskutusApplication.dao.CompanyDao;
 import fi.metropolia.LaskutusApplication.dao.UserDao;
 import fi.metropolia.LaskutusApplication.model.DAOCompany;
 import fi.metropolia.LaskutusApplication.model.DAOUser;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 public class UserDTOTest {
 	@TestConfiguration
 	static class UserNameConfigurationTest {
@@ -28,7 +30,7 @@ public class UserDTOTest {
 	@MockBean
 	private UserDao userRepository;
 //setup username
-	@Before
+	@BeforeEach
 	public void setUp() {
 		DAOUser alex = new DAOUser();
 		alex.setUsername("alex");
